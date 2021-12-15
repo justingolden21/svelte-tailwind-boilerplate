45e40b6166e6786c3f468a0a89280182b4de7498
Justin Golden <justingolden21@gmail.com>
2021-12-14 19:59:26 -0800

    simplify config/app

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,14 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter(),
+		adapter: adapter()
 	},
 
 	preprocess: [
 		preprocess({
-			postcss: true,
-		}),
-	],
+			postcss: true
+		})
+	]
 };
 
 export default config;
